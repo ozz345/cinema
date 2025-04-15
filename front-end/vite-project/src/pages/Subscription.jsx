67e057161e6a1a333f88e3ae15adb1beaaddc0c4
Oz <ozz345@gmail.com>
2025-04-15 13:21:09 +0300

    PWA

--- front-end/vite-project/src/pages/Subscription.jsx
+++ front-end/vite-project/src/pages/Subscription.jsx
@@ -12,7 +12,7 @@ const Subscription = () => {
 
 <>
 <div>
-<h1>Subscriptions page Lists:</h1>
+<h1>Subscriptions page Lists PWA:</h1>
 <nav style={{
         backgroundColor: '#6743',
         padding: '1rem',
